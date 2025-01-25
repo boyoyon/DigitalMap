b27cd1c84ce64ddb441b6de3eee657dc54b7ec3f
boyoyon <nishimaki.hidetoshi@gmail.com>
2025-01-26 08:22:47 +0900

    Add files via upload

--- src/o3d_display_mesh.py
+++ src/o3d_display_mesh.py
@@ -7,15 +7,14 @@ def main():
     argv = sys.argv
     argc = len(argv)
 
+    print('%s displays mesh file (.ply)' % argv[0])
+    print('[usage] python %s <.ply file>' % argv[0])
+
     if argc < 2:
-        print('%s loads and displays mesh file (.obj)' % argv[0])
-        print('%s <obj file>' % argv[0])
         quit()
 
     mesh =  o3d.io.read_triangle_mesh(argv[1])
-
     mesh.compute_vertex_normals()
-
     o3d.visualization.draw_geometries([mesh])
 
 if __name__ == '__main__':
